34078a59e77552849f21394f15e2b4f9c66f9d68
Joas <joas.madragule.jm@gmail.com>
2021-06-11 05:42:11 +0300

    Just added fragmements to the MEOW-BOTTOM-NAVIGATION

--- app/src/main/java/com/example/finalprojectassistance/Admin_Dashboard.java
+++ app/src/main/java/com/example/finalprojectassistance/Admin_Dashboard.java
@@ -1,14 +1,56 @@
 package com.example.finalprojectassistance;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
 public class Admin_Dashboard extends AppCompatActivity {
+
+    //initializing
+    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        //Assigning variables
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        //adding menu item
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_notifications_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_account_circle_24));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                // your codes
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                // your codes
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                // your codes
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
